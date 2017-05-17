78075f82db3197f1bb341f1eb7257c1645ce99d3
leo <weiwei.li@quncaotech.com>
2017-05-17 11:52:34 +0800

    3.0.1

---
+++ Demo/Demo/LARefresh/LARefreshFooter.h
@@ -0,0 +1,15 @@
+//
+//  LARefreshFooter.h
+//  Pods
+//
+//  Created by leo on 16/11/24.
+//
+//
+
+
+#import <MJRefresh/MJRefreshFooter.h>
+#import <MJRefresh/MJRefreshAutoFooter.h>
+
+@interface LARefreshFooter : MJRefreshAutoFooter
+
+@end
