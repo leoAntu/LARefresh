fe336910ae5e340e4db4945afcc7f07f972beb4c
leo <weiwei.li@quncaotech.com>
2017-05-17 11:15:38 +0800

    2.0.6

--- LARefresh/LARefreshHeader.h
+++ LARefresh/LARefreshHeader.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 QuanYan. All rights reserved.
 //
 #import <UIKit/UIKit.h>
-#import <MJRefresh/MJRefresh.h>
+#import <MJRefresh/MJRefreshHeader.h>
 typedef enum {
     BFRefreshHeaderStyleDefault        = 0,
     BFRefreshHeaderStyleAtTop          = 1,//图片置顶时，下拉放大图片刷新
